7a65446604d1be2eff927cd7f99e51e5df369808
Wuotto-D <danielwuotto@gmail.com>
2023-09-10 22:47:33 -0600

    Actividad 5

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
+const { pokemon } = require('./pokedex.json')
 
 app.get("/", (req, res, next)=> {
     res.status(200);
-    res.send("Bienvenido");
+    res.send("Bienvenidos al Pokedex");
+});
+app.get('/pokemon/all', (req, res, next) =>{
+    res.status(200);
+    res.send(pokemon);
 });
 
-app.listen(3000, () =>{
+app.get('/pokemon/:id([0-9]{1,3})',(req, res, next) => {
+    const id = req.params.id-1;
+    if(id>=0 && id <= 150){
+        res.status(200);
+       return res.send(pokemon[req.params.id - 1]);
+    }
+});
+
+app.get('/pokemon/:name', (req, res, next)=>{
+    const name = req.params.name;
+    for(i=0; i<pokemon.length; i++){
+        if(pokemon[i].name == name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(400);
+    res.send("Pokémon no encontrado")
+});
+
+app.listen(process.env.PORT || 3000, () =>{
     console.log("Server is running...");
 });
