517d71785c0e5c5bdad66f40037d989b726c1805
Wuotto-D <danielwuotto@gmail.com>
2023-09-24 20:45:05 -0600

    added pokemon POST

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next)=> {
-   return res.status(200).json({code: 1, mesage: "Bienvenido al Pokedex"});
+   return res.status(200).json({code: 1, message: "Bienvenido al Pokedex"});
 });
 
 app.use("/pokemon", pokemon);
 
 app.use((req, res, next) =>{
-    return res.status(404).json({code: 404, mesage: "URL no encontrada"});
+    return res.status(404).json({code: 404, message: "URL no encontrada"});
 });
 
 app.listen(process.env.PORT || 3000, () =>{
